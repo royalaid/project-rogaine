47ffb31bf592c26c50ee1bd26a7cd517d7e503a9
Royalaid <2439803+royalaid@users.noreply.github.com>
2024-04-12 16:51:44 -0700

    image updates

--- forehead/src/app/page.tsx
+++ forehead/src/app/page.tsx
@@ -1,7 +1,6 @@
 import { getFrameMetadata } from "frog/next";
 import type { Metadata } from "next";
 import React from "react";
-import { Heading, NumberedList, Title } from "./components/text";
 import "./globals.css";
 import "./Home.module.css";
 import "./style.css";
@@ -18,24 +17,30 @@ function App() {
   const minting = false;
   if(minting){
     return (
-      <>
-      <div className="container text-center h-[90%] md:h-auto">
+    <div className="container h-[90%] text-center md:h-auto">
         <div className="meme-info mx-auto my-5">
-          <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHFjdDIzZGF1Z3h1N2M2Y3A3Z3phd2x1d2RleGFsczg4eTY0djE2dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/eJEPRwpQMjGDmd6nvt/giphy.gif" alt="Meme" className="mx-auto" />
-          <p className="description">This meme represents the spirit of the 90s internet culture.</p>
-          <p className="donation">Donations go to: Digital Art Preservation Fund</p>
+        <img
+          src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHFjdDIzZGF1Z3h1N2M2Y3A3Z3phd2x1d2RleGFsczg4eTY0djE2dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/eJEPRwpQMjGDmd6nvt/giphy.gif"
+          alt="Meme"
+          className="mx-auto"
+        />
+        <p className="description">
+          This meme represents the spirit of the 90s internet culture.
+        </p>
+        <p className="donation">
+          Donations go to: Digital Art Preservation Fund
+        </p>
           <p className="mint-info">Minted: 150 times</p>
           <p className="total-raised">Total Raised: $3000</p>
-          <button className="buy-button block mx-auto my-5">Buy</button>
+        <button className="buy-button mx-auto my-5 block">Buy</button>
         </div>
-        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
+      <div className="absolute bottom-10 left-1/2 -translate-x-1/2 transform">
           <button className="buy-button">Learn More</button>
         </div>
         <div className="absolute bottom-10 right-5">
           <button className="buy-button rounded-full">Random</button>
         </div>
       </div>
-      </>
     );
   } else{
     return (
