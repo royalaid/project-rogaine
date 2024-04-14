511a9675ca4efaa1730b69bb774495a0c865d24e
Pablo <pablo@hashingsystems.com>
2024-04-14 00:05:32 -0500

    Update page.tsx

--- forehead/src/app/page.tsx
+++ forehead/src/app/page.tsx
@@ -15,26 +15,43 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 function App() {
-  return (
-    <>
-    <div className="container text-center h-[90%] md:h-auto">
-      <div className="meme-info mx-auto my-5">
-        <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHFjdDIzZGF1Z3h1N2M2Y3A3Z3phd2x1d2RleGFsczg4eTY0djE2dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/eJEPRwpQMjGDmd6nvt/giphy.gif" alt="Meme" className="mx-auto" />
-        <p className="description">This meme represents the spirit of the 90s internet culture.</p>
-        <p className="donation">Donations go to: Digital Art Preservation Fund</p>
-        <p className="mint-info">Minted: 150 times</p>
-        <p className="total-raised">Total Raised: $3000</p>
-        <button className="buy-button block mx-auto my-5">Buy</button>
+  const minting = false;
+  if(minting){
+    return (
+      <>
+      <div className="container text-center h-[90%] md:h-auto">
+        <div className="meme-info mx-auto my-5">
+          <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHFjdDIzZGF1Z3h1N2M2Y3A3Z3phd2x1d2RleGFsczg4eTY0djE2dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/eJEPRwpQMjGDmd6nvt/giphy.gif" alt="Meme" className="mx-auto" />
+          <p className="description">This meme represents the spirit of the 90s internet culture.</p>
+          <p className="donation">Donations go to: Digital Art Preservation Fund</p>
+          <p className="mint-info">Minted: 150 times</p>
+          <p className="total-raised">Total Raised: $3000</p>
+          <button className="buy-button block mx-auto my-5">Buy</button>
+        </div>
+        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
+          <button className="buy-button">Learn More</button>
+        </div>
+        <div className="absolute bottom-10 right-5">
+          <button className="buy-button rounded-full">Random</button>
+        </div>
       </div>
-      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
-        <button className="buy-button">Learn More</button>
+      </>
+    );
+  } else{
+    return (
+      <div className="container text-center h-[90%] md:h-auto">
+        <div className="meme-info mx-auto my-5">
+          <h1 className="text-5xl font-bold">Welcome to Regen Games!</h1>
+          <p className="description">Creating positive-sum games that give back to the community and environment.</p>
+          <div className="flex justify-center gap-4 my-5">
+            <a href="https://twitter.com" className="link link-primary">Twitter</a>
+            <a href="https://facebook.com" className="link link-secondary">Facebook</a>
+            <a href="https://instagram.com" className="link link-accent">Instagram</a>
+          </div>
+        </div>
       </div>
-      <div className="absolute bottom-10 right-5">
-        <button className="buy-button rounded-full">Random</button>
-      </div>
-    </div>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
