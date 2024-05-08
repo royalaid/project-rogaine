f9ad5ab6b15ca9c9019769a557eb0b133789481d
Pablo The Penguin <pablo12335@gmail.com>
2024-05-08 10:59:19 +0200

    update aerobond variables

--- contracts/AeroBond.sol
+++ contracts/AeroBond.sol
@@ -7,47 +7,47 @@ import "./IGauge.sol";
 contract AeroBond {
     address public manager;
     address public treasury;
-    address public guardian;
 
-    IGauge public constant tokenGauge = IGauge(0xeAE066C25106006fB386A3a8b1698A0cB6931c1a);
-    IERC20 public constant LP_TOKEN = IERC20(0x0B25c51637c43decd6CC1C1e3da4518D54ddb528);
-    IERC20 public constant AERO = IERC20(0x940181a94A35A4569E4529A3CDfB74e38FD98631);
-    IRouter public constant router = IRouter(0xcF77a3Ba9A5CA399B7c97c74d54e5b1Beb874E43);
-    IERC20 public constant TOKEN = IERC20(0x4621b7A9c75199271F773Ebd9A499dbd165c3191);
-    IERC20 public constant WETH = IERC20(0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA);
-    address public constant factory = 0x420DD381b31aEf6683db6B902084cB0FFECe40Da;
+    IGauge public constant tokenGauge = IGauge(0xeAE066C25106006fB386A3a8b1698A0cB6931c1a); // STILL NEED
+
+    IERC20 public constant LP_TOKEN = IERC20(0x11D9944cB1886F5Ca08673C0B61b4d159946AcDa); // REGEN/WETH
+
+    IERC20 public constant AERO = IERC20(0x940181a94A35A4569E4529A3CDfB74e38FD98631); // AERO
+    IRouter public constant router = IRouter(0xcF77a3Ba9A5CA399B7c97c74d54e5b1Beb874E43); // Router
+    IERC20 public constant TOKEN = IERC20(0x1D653f09f216682eDE4549455D6Cf45f93C730cf); // REGEN
+    IERC20 public constant WETH = IERC20(0x4200000000000000000000000000000000000006); // WETH
 
     error OnlyManagement();
-    error OnlyGuardian();
 
-    error MaxSlippageTooHigh();
-    error NotEnoughTokens();
-    error LiquiditySlippageTooHigh();
-
-    modifier onlyGuardian() {
-        if(msg.sender!=guardian){
-            revert OnlyGuardian();
+    modifier onlyManager() {
+        if(msg.sender!=manager){
+            revert OnlyManagement();
         }
         _;
     }
 
     constructor(
         address treasury_,
-        address manager_,
-        address guardian_
+        address manager_
     )
     {   
         // recieves the rewards
         treasury = treasury_;
         // manages the liquidity
         manager = manager_;
-        // manages control
-        guardian = guardian_;
     }
 
     function claimAeroRewards() external {
         tokenGauge.getReward(address(this));
         AERO.transfer(treasury, AERO.balanceOf(address(this)));
+    }
+
+    function changeTreasury(address newTreasury_) onlyManager external {
+        treasury = newTreasury_;
+    }
+
+    function transferTokens(address token) onlyManager external {
+        IERC20(token).transfer(treasury, IERC20(token).balanceOf(address(this)));
     }
 
     function deposit(uint wethAmount) public {
@@ -63,16 +63,5 @@ contract AeroBond {
         TOKEN.transfer(msg.sender, tokenSpent);
 
         tokenGauge.deposit(LP_TOKEN.balanceOf(address(this)));
-    }
-
-    function getRoute(address from, address to) internal pure returns(IRouter.Route[] memory){
-        IRouter.Route memory route = IRouter.Route(from, to, true, factory);
-        IRouter.Route[] memory routeArray = new IRouter.Route[](1);
-        routeArray[0] = route;
-        return routeArray;
-    }
-
-    function changeTreasury(address newTreasury_) onlyGuardian external {
-        treasury = newTreasury_;
     }
 }
